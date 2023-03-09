0011aebb8a42b6120a8617c937e20a8061909c80
Pavitra Patel <Pavs.network7@gmail.com>
2023-03-08 21:35:46 -0500

    Change to Knight.java

--- Knight.java
+++ Knight.java
@@ -4,19 +4,39 @@ public class Knight extends Board
 {
 	//class variables
 	private String color;
-
+	private String name;
+	
 	//constructors
 	public Knight()
 	{
 		color="";
 	}
-
+	
+	public boolean isValid(Board[][] board, int initiali, int initialj, int finali, int finalj)
+	{
+		//TODO
+		return true;
+	}
+	
+	public Board move(Board obj)
+	{
+		obj = new Knight();
+		obj.setColor(this.getColor());
+		obj.setName(this.getName());
+		return obj;
+	}
+	
 	//set methods
 	public void setColor(String c)
 	{
 		color=c;
+		name="N";
 	}
-
+	public void setName (String n)
+	{
+		name=n;
+	}
+	
 	//get methods
 	public String getColor()
 	{
@@ -24,6 +44,6 @@ public class Knight extends Board
 	}
 	public String getName()
 	{
-		return "N";
+		return name;
 	}
 }
